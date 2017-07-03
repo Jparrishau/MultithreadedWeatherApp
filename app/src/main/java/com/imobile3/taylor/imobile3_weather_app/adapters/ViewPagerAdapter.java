4aa2028f75923d5bdd733bd68684d996b9fca2f0
Joseph Taylor Parrish <jparrishau@gmail.com>
2017-07-03 16:32:01 -0500

    Added a ViewPager Adapter. Will need tweaking more.

---
+++ app/src/main/java/com/imobile3/taylor/imobile3_weather_app/adapters/ViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.imobile3.taylor.imobile3_weather_app.adapters;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by taylorp on 7/3/2017.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Today";
+            case 1:
+                return "Tomorrow";
+            case 2:
+                return "Later";
+        }
+        return null;
+    }
+}
