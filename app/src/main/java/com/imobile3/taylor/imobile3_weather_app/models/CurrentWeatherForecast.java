5e3fcfcca752d3597590f6cce64122dba4b3c34b
Joseph Taylor Parrish <jparrishau@gmail.com>
2017-07-03 16:39:18 -0500

    Generic changes.

--- app/src/main/java/com/imobile3/taylor/imobile3_weather_app/models/CurrentWeatherForecast.java
+++ app/src/main/java/com/imobile3/taylor/imobile3_weather_app/models/CurrentWeatherForecast.java
@@ -1,15 +1,17 @@
 package com.imobile3.taylor.imobile3_weather_app.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by taylorp on 7/3/2017.
  */
-
-public class CurrentWeatherForecast {
+public class CurrentWeatherForecast implements Parcelable {
     private String mWeather_description;
-    private String mCurrentTempText;
+    private String mTempText;
 
-    private double mCurrentTemp_F;
-    private double mCurrentTemp_C;
+    private double mTemp_F;
+    private double mTemp_C;
     private String mHumidity;
 
     private String mWind_text;
@@ -21,23 +23,23 @@ public class CurrentWeatherForecast {
     private double mWind_gust_kph;
 
 
-    public CurrentWeatherForecast(String weather_description, String currentTempText,
-                                  double currentTemp_F, double currentTemp_C,
+    public CurrentWeatherForecast(String weather_description, String tempText,
+                                  double temp_F, double temp_C,
                                   String humidity, String wind_text, String wind_dir,
                                   int wind_degrees, double wind_mph, double wind_gust_mph,
                                   double wind_kph, double wind_gust_kph) {
-        mWeather_description = weather_description;
-        mCurrentTempText = currentTempText;
-        mCurrentTemp_F = currentTemp_F;
-        mCurrentTemp_C = currentTemp_C;
-        mHumidity = humidity;
-        mWind_text = wind_text;
-        mWind_dir = wind_dir;
-        mWind_degrees = wind_degrees;
-        mWind_mph = wind_mph;
-        mWind_gust_mph = wind_gust_mph;
-        mWind_kph = wind_kph;
-        mWind_gust_kph = wind_gust_kph;
+        setWeather_description(weather_description);
+        setTempText(tempText);
+        setTemp_F(temp_F);
+        setTemp_C(temp_C);
+        setHumidity(humidity);
+        setWind_text(wind_text);
+        setWind_dir(wind_dir);
+        setWind_degrees(wind_degrees);
+        setWind_mph(wind_mph);
+        setWind_gust_mph(wind_gust_mph);
+        setWind_kph(wind_kph);
+        setWind_kph(wind_gust_kph);
     }
 
     public String getWeather_description() {
@@ -48,28 +50,28 @@ public class CurrentWeatherForecast {
         mWeather_description = weather_description;
     }
 
-    public String getCurrentTempText() {
-        return mCurrentTempText;
+    public String getTempText() {
+        return mTempText;
     }
 
-    public void setCurrentTempText(String currentTempText) {
-        mCurrentTempText = currentTempText;
+    public void setTempText(String tempText) {
+        mTempText = tempText;
     }
 
-    public double getCurrentTemp_F() {
-        return mCurrentTemp_F;
+    public double getTemp_F() {
+        return mTemp_F;
     }
 
-    public void setCurrentTemp_F(double currentTemp_F) {
-        mCurrentTemp_F = currentTemp_F;
+    public void setTemp_F(double temp_F) {
+        mTemp_F = temp_F;
     }
 
-    public double getCurrentTemp_C() {
-        return mCurrentTemp_C;
+    public double getTemp_C() {
+        return mTemp_C;
     }
 
-    public void setCurrentTemp_C(double currentTemp_C) {
-        mCurrentTemp_C = currentTemp_C;
+    public void setTemp_C(double temp_C) {
+        mTemp_C = temp_C;
     }
 
     public String getHumidity() {
@@ -136,4 +138,51 @@ public class CurrentWeatherForecast {
         mWind_gust_kph = wind_gust_kph;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mWeather_description);
+        dest.writeString(this.mTempText);
+        dest.writeDouble(this.mTemp_F);
+        dest.writeDouble(this.mTemp_C);
+        dest.writeString(this.mHumidity);
+        dest.writeString(this.mWind_text);
+        dest.writeString(this.mWind_dir);
+        dest.writeInt(this.mWind_degrees);
+        dest.writeDouble(this.mWind_mph);
+        dest.writeDouble(this.mWind_gust_mph);
+        dest.writeDouble(this.mWind_kph);
+        dest.writeDouble(this.mWind_gust_kph);
+    }
+
+    protected CurrentWeatherForecast(Parcel in) {
+        this.mWeather_description = in.readString();
+        this.mTempText = in.readString();
+        this.mTemp_F = in.readDouble();
+        this.mTemp_C = in.readDouble();
+        this.mHumidity = in.readString();
+        this.mWind_text = in.readString();
+        this.mWind_dir = in.readString();
+        this.mWind_degrees = in.readInt();
+        this.mWind_mph = in.readDouble();
+        this.mWind_gust_mph = in.readDouble();
+        this.mWind_kph = in.readDouble();
+        this.mWind_gust_kph = in.readDouble();
+    }
+
+    public static final Parcelable.Creator<CurrentWeatherForecast> CREATOR = new Parcelable.Creator<CurrentWeatherForecast>() {
+        @Override
+        public CurrentWeatherForecast createFromParcel(Parcel source) {
+            return new CurrentWeatherForecast(source);
+        }
+
+        @Override
+        public CurrentWeatherForecast[] newArray(int size) {
+            return new CurrentWeatherForecast[size];
+        }
+    };
 }
