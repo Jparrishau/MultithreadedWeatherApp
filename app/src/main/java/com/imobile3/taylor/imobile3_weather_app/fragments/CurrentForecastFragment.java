f3275081fbaacb42efbc19f8413bee15dfe4abe4
Joseph Taylor Parrish <jparrishau@gmail.com>
2017-07-04 19:52:21 -0500

    Set the fields for the Current Weather AppBar.

--- app/src/main/java/com/imobile3/taylor/imobile3_weather_app/fragments/CurrentForecastFragment.java
+++ app/src/main/java/com/imobile3/taylor/imobile3_weather_app/fragments/CurrentForecastFragment.java
@@ -10,10 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.imobile3.taylor.imobile3_weather_app.R;
-import com.imobile3.taylor.imobile3_weather_app.activities.DetailedForecastActivity;
-import com.imobile3.taylor.imobile3_weather_app.adapters.ForecastAdapter;
+import com.imobile3.taylor.imobile3_weather_app.activities.DetailedWeatherForecastActivity;
+import com.imobile3.taylor.imobile3_weather_app.adapters.SimpleForecastAdapter;
 import com.imobile3.taylor.imobile3_weather_app.models.DailyDetailedWeatherItem;
 import com.imobile3.taylor.imobile3_weather_app.models.Location;
 
@@ -22,25 +23,25 @@ import java.util.ArrayList;
 /**
  * Created by Taylor Parrish on 8/29/2016
  *
- * SimpleForecastFragment runs a background task using the users location
+ * CurrentForecastFragment runs a background task using the users location
  * data that searches for relevant weather details to then be displayed to the user.
  */
-public class SimpleForecastFragment extends Fragment {
-    private static final String CLASS_TAG = SimpleForecastFragment.class.getSimpleName();
+public class CurrentForecastFragment extends Fragment {
+    private static final String CLASS_TAG = CurrentForecastFragment.class.getSimpleName();
     private static final boolean DEBUG = true;
 
-    private boolean mIsTaskRunning = false;
-
-    private Bundle mSavedLocation;
     public final static String TAG_EXTRA_DETAIL_ITEMS = "detailWeatherItems";
     public final static String TAG_LOCATION_BUNDLE = "locationBundle";
+
+    Location mLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         if (DEBUG) Log.d(CLASS_TAG, "onCreate(Bundle)");
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        mSavedLocation = savedInstanceState;
+        mLocation =
+                getActivity().getIntent().getParcelableExtra(TAG_LOCATION_BUNDLE);
     }
 
     @Override
@@ -48,34 +49,51 @@ public class SimpleForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         if (DEBUG) Log.d(CLASS_TAG, "onCreateView(LayoutInflater, ViewGroup, Bundle)");
 
-        return inflater.inflate(R.layout.forecast_fragment, container, false);
+        String currentTemp = mLocation.getCurrentWeatherForecast().getTemp_F() + " °F";
+        String currentWeatherDescr = mLocation.getCurrentWeatherForecast().getWeather_description();
+        String currentPressure = "1016.0 hPa";
+        String currentHumidity = mLocation.getCurrentWeatherForecast().getHumidity();
+        Double currentWindSpeed = mLocation.getCurrentWeatherForecast().getWind_mph();
+
+        View view = inflater.inflate(R.layout.forecast_fragment, container, false);
+        TextView currentTempText = (TextView) view.findViewById(R.id.currentTemperature);
+        TextView currentWeatherDescrText = (TextView) view.findViewById(R.id.currentWeatherDescription);
+        TextView currentPressureText = (TextView) view.findViewById(R.id.currentPressure);
+        TextView currentHumidityText = (TextView) view.findViewById(R.id.currentHumidity);
+        TextView currentWindSpeedText = (TextView) view.findViewById(R.id.currentWind);
+        TextView sunriseText = (TextView) view.findViewById(R.id.todaySunrise);
+        TextView sunsetText = (TextView) view.findViewById(R.id.todaySunset);
+
+        currentTempText.setText(currentTemp);
+        currentWeatherDescrText.setText(currentWeatherDescr);
+        currentWindSpeedText.setText("Wind: " + currentWindSpeed + " Mph");
+        currentPressureText.setText("Pressure: " + currentPressure);
+        currentHumidityText.setText("Humidity: " + currentHumidity);
+        sunriseText.setText("Sunrise: " + "5:42am");
+        sunsetText.setText("Sunset: " + "8:00pm");
+        return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         if (DEBUG) Log.d(CLASS_TAG, "onActivityCreated(Bundle)");
         super.onActivityCreated(savedInstanceState);
-
-            Location location =
-                getActivity().getIntent().getParcelableExtra(TAG_LOCATION_BUNDLE);
-            setupWeatherItemListView(location);
-
+        setupWeatherItemListView(mLocation);
     }
-
     private void setupWeatherItemListView(final Location location) {
         ListView simpleForecastListview = (ListView) getActivity().findViewById(R.id.forecastListView);
         simpleForecastListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                Intent detailedforecastIntent = new Intent(getActivity(), DetailedForecastActivity.class);
+                Intent detailedforecastIntent = new Intent(getActivity(), DetailedWeatherForecastActivity.class);
                 ArrayList<DailyDetailedWeatherItem> detailItems = location.getDay(position).getWeatherForecast().getDetailWeatherItems();
 
                 detailedforecastIntent.putParcelableArrayListExtra(TAG_EXTRA_DETAIL_ITEMS, detailItems);
                 startActivity(detailedforecastIntent);
             }
         });
-        ForecastAdapter adapter = new ForecastAdapter(getActivity(), location);
+        SimpleForecastAdapter adapter = new SimpleForecastAdapter(getActivity(), location);
         simpleForecastListview.setAdapter(adapter);
     }
 }
