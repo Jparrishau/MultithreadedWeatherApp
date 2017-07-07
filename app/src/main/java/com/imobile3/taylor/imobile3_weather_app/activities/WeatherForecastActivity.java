d71fc31761a3d6d154b6bd69c9571a1e24ed5db4
Joseph Taylor Parrish <jparrishau@gmail.com>
2017-07-07 16:59:50 -0500

    Removed currentWeatherForecast fragment and migrated it to the activity level.

--- app/src/main/java/com/imobile3/taylor/imobile3_weather_app/activities/WeatherForecastActivity.java
+++ app/src/main/java/com/imobile3/taylor/imobile3_weather_app/activities/WeatherForecastActivity.java
@@ -1,15 +1,27 @@
 package com.imobile3.taylor.imobile3_weather_app.activities;
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.imobile3.taylor.imobile3_weather_app.R;
-import com.imobile3.taylor.imobile3_weather_app.fragments.CurrentForecastFragment;
+import com.imobile3.taylor.imobile3_weather_app.adapters.SimpleForecastAdapter;
+import com.imobile3.taylor.imobile3_weather_app.adapters.ViewPagerAdapter;
 import com.imobile3.taylor.imobile3_weather_app.fragments.WeatherForecastFragment;
+import com.imobile3.taylor.imobile3_weather_app.models.DailyDetailedWeatherItem;
+import com.imobile3.taylor.imobile3_weather_app.models.Location;
+
+import java.util.ArrayList;
 
 /**
  * Created by Taylor Parrish on 8/23/2016.
@@ -25,31 +37,62 @@ public class WeatherForecastActivity extends AppCompatActivity {
     private static final boolean DEBUG = true;
 
     private static final String TAG_SIMPLE_FRAGMENT = "simple_fragment";
-    private CurrentForecastFragment mCurrentForecastFragment;
+    public final static String TAG_LOCATION_BUNDLE = "locationBundle";
+    public final static String TAG_EXTRA_DETAIL_ITEMS = "detailWeatherItems";
+
     private WeatherForecastFragment mWeatherForecastFragment;
+
+    private Location mLocation;
+
+    private ViewPager mPager;
+    private ViewPagerAdapter mPagerAdapter;
+
+    TextView currentTempText;
+    TextView currentWeatherDescrText;
+    TextView currentPressureText;
+    TextView currentHumidityText;
+    TextView currentWindSpeedText;
+    TextView sunriseText;
+    TextView sunsetText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (DEBUG) Log.i(CLASS_TAG, "onCreate(Bundle)");
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_forecast);
 
-        if (savedInstanceState == null) {
-            mCurrentForecastFragment = new CurrentForecastFragment();
-            mWeatherForecastFragment = new WeatherForecastFragment();
-
-            getSupportFragmentManager().beginTransaction()
-                    .add(android.R.id.content, mCurrentForecastFragment)
-                    .commit();
-        } else {
-            mCurrentForecastFragment = (CurrentForecastFragment) getSupportFragmentManager().findFragmentByTag(TAG_SIMPLE_FRAGMENT);
-        }
+        mLocation =
+                getIntent().getParcelableExtra(TAG_LOCATION_BUNDLE);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.forecast_toolbar);
         if (toolbar != null)
         {
-            this.setSupportActionBar(toolbar);//To display toolbar
-            getSupportActionBar().setElevation(2); // or other...
+            this.setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(mLocation.getCity() + ", " + mLocation.getState());
         }
+
+        currentTempText = (TextView) findViewById(R.id.currentTemperature);
+        currentWeatherDescrText = (TextView) findViewById(R.id.currentWeatherDescription);
+        currentPressureText = (TextView) findViewById(R.id.currentPressure);
+        currentHumidityText = (TextView) findViewById(R.id.currentHumidity);
+        currentWindSpeedText = (TextView) findViewById(R.id.currentWind);
+        sunriseText = (TextView) findViewById(R.id.todaySunrise);
+        sunsetText = (TextView) findViewById(R.id.todaySunset);
+
+        String currentTemp = mLocation.getCurrentWeatherForecast().getTemp_F() + " °F";
+        String currentWeatherDescr = mLocation.getCurrentWeatherForecast().getWeather_description();
+        String currentPressure = "1016.0 hPa";
+        String currentHumidity = mLocation.getCurrentWeatherForecast().getHumidity();
+        Double currentWindSpeed = mLocation.getCurrentWeatherForecast().getWind_mph();
+
+        currentTempText.setText(currentTemp);
+        currentWeatherDescrText.setText(currentWeatherDescr);
+        currentWindSpeedText.setText("Wind: " + currentWindSpeed + " Mph");
+        currentPressureText.setText("Pressure: " + currentPressure);
+        currentHumidityText.setText("Humidity: " + currentHumidity);
+        sunriseText.setText("Sunrise: " + "5:42am");
+        sunsetText.setText("Sunset: " + "8:00pm");
+        setupWeatherItemListView(mLocation);
     }
 
     //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -82,6 +125,23 @@ public class WeatherForecastActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private void setupWeatherItemListView(final Location location) {
+        ListView simpleForecastListview = (ListView) findViewById(R.id.forecastListView);
+        simpleForecastListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                Intent detailedforecastIntent = new Intent(view.getContext(), DetailedWeatherForecastActivity.class);
+                ArrayList<DailyDetailedWeatherItem> detailItems = location.getDay(position).getWeatherForecast().getDetailWeatherItems();
+
+                detailedforecastIntent.putParcelableArrayListExtra(TAG_EXTRA_DETAIL_ITEMS, detailItems);
+                startActivity(detailedforecastIntent);
+            }
+        });
+        SimpleForecastAdapter adapter = new SimpleForecastAdapter(this, location);
+        simpleForecastListview.setAdapter(adapter);
     }
 }
 
