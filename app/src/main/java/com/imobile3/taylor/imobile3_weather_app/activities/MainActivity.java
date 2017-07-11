c70bb9fcdf097f3c23621b31833a0e8e3f937d70
Joseph Taylor Parrish <jparrishau@gmail.com>
2017-07-11 16:28:17 -0500

    Added FAB button to activity level.

--- app/src/main/java/com/imobile3/taylor/imobile3_weather_app/activities/MainActivity.java
+++ app/src/main/java/com/imobile3/taylor/imobile3_weather_app/activities/MainActivity.java
@@ -1,11 +1,14 @@
 package com.imobile3.taylor.imobile3_weather_app.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.imobile3.taylor.imobile3_weather_app.R;
 import com.imobile3.taylor.imobile3_weather_app.fragments.PastLocationFragment;
@@ -47,6 +50,17 @@ public class MainActivity extends AppCompatActivity {
         {
             this.setSupportActionBar(toolbar);
         }
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
 
     //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
