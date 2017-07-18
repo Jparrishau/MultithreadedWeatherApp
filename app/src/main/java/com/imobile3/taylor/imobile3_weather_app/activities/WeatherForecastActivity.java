d9fe7d9e967d1ef3de96c59c70f6a4f2d65f6ee4
Joseph Taylor Parrish <jparrishau@gmail.com>
2017-07-18 17:06:11 -0500

    Added dialog for data task

--- app/src/main/java/com/imobile3/taylor/imobile3_weather_app/activities/WeatherForecastActivity.java
+++ app/src/main/java/com/imobile3/taylor/imobile3_weather_app/activities/WeatherForecastActivity.java
@@ -1,7 +1,10 @@
 package com.imobile3.taylor.imobile3_weather_app.activities;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +19,12 @@ import com.google.gson.Gson;
 import com.imobile3.taylor.imobile3_weather_app.R;
 import com.imobile3.taylor.imobile3_weather_app.adapters.ViewPagerAdapter;
 import com.imobile3.taylor.imobile3_weather_app.fragments.RecyclerViewFragment;
+import com.imobile3.taylor.imobile3_weather_app.interfaces.WeatherDataTaskListener;
 import com.imobile3.taylor.imobile3_weather_app.models.Location;
+import com.imobile3.taylor.imobile3_weather_app.tasks.WeatherDataLookup;
 import com.imobile3.taylor.imobile3_weather_app.utilities.Utils;
+
+import java.util.Calendar;
 
 /**
  * WeatherForecastActivity is responsible for loading its corresponding fragment
@@ -29,11 +36,15 @@ import com.imobile3.taylor.imobile3_weather_app.utilities.Utils;
  * @author Taylor Parrish
  * @since 8/23/2016
  */
-public class WeatherForecastActivity extends AppCompatActivity {
+public class WeatherForecastActivity extends AppCompatActivity implements WeatherDataTaskListener {
     private static final String CLASS_TAG = WeatherForecastActivity.class.getSimpleName();
     private static final boolean DEBUG = true;
 
     public final static String TAG_LOCATION_BUNDLE = "locationBundle";
+
+    private SharedPreferences mSharedPreferences;
+    private boolean mIsTaskRunning = false;
+    private ProgressDialog mProgressDialog;
 
     private Location mLocation;
     private ViewPager mPager;
@@ -58,6 +69,8 @@ public class WeatherForecastActivity extends AppCompatActivity {
         mLocation =
                 getIntent().getParcelableExtra(TAG_LOCATION_BUNDLE);
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.forecast_toolbar);
         if (toolbar != null)
         {
@@ -65,11 +78,72 @@ public class WeatherForecastActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(mLocation.getCity() + ", " + mLocation.getState());
         }
 
-        setupForecastView();
+        setupForecastPager();
         setupCurrentForecastBar();
     }
 
-    private void setupForecastView() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isTimeToUpdate();
+    }
+
+    //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_forecast, menu);
+        return true;
+    }
+
+    //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                //Do something
+                return true;
+            case R.id.action_refresh:
+                new WeatherDataLookup(this, this).execute(mLocation);
+                return true;
+            case R.id.action_search:
+                //Do something
+                return true;
+            case R.id.action_location:
+                //Do something
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onTaskStarted() {
+        mIsTaskRunning = true;
+        mProgressDialog = ProgressDialog.show(this, "Fetching data", "Please wait...");
+    }
+
+    @Override
+    public void onWeatherDataTaskFinished(Location location) {
+        location.updateTimeStamp();
+        refreshUI(location);
+
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+        mIsTaskRunning = false;
+    }
+
+    private void refreshUI(Location location) {
+        mLocation = location;
+        setupForecastPager();
+        setupCurrentForecastBar();
+        saveLocationData(location);
+    }
+
+    private void setupForecastPager() {
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.viewPager);
         mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -136,37 +210,19 @@ public class WeatherForecastActivity extends AppCompatActivity {
         mSunsetText.setText("Sunset: " + sunsetTime);
     }
 
-    //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_forecast, menu);
-        return true;
+    private void saveLocationData(Location location) {
+        Gson gson = new Gson();
+        String locationJSON = gson.toJson(location);
+        mSharedPreferences.edit().putString(location.getCoordinates(), locationJSON).apply();
     }
 
-    //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                //Do something
-                return true;
-            case R.id.action_refresh:
-                //Do something
-                return true;
-            case R.id.action_search:
-                //Do something
-                return true;
-            case R.id.action_location:
-                //Do something
-                return true;
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
+    private void isTimeToUpdate(){
+        long timeElapsed =
+                (Calendar.getInstance().getTimeInMillis() - mLocation.getTimeStamp().getTimeInMillis());
+        if(timeElapsed >= 3600000){
+            new WeatherDataLookup(this, this).execute(mLocation);
         }
     }
-
 }
 
 
